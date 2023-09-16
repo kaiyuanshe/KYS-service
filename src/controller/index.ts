c50f9100b74d2139d18fedf2f0052a1971170b93
TechQuery <shiy2008@gmail.com>
2023-09-17 05:20:03 +0800

    [fix] 3 detail Publishing bug

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,10 +1,11 @@
 import type {} from 'koa2-swagger-ui';
 import { createAPI } from 'koagger';
 
+import { CheckEventController } from './CheckEvent';
 import { CrawlerController } from './Crawler';
 import { SessionController } from './Session';
 
 export const { router, swagger, mocker } = createAPI({
     mock: true,
-    controllers: [CrawlerController, SessionController]
+    controllers: [SessionController, CheckEventController, CrawlerController]
 });
